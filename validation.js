e3d32a81b0f0f7646b8b6547b8bb04e64d66c27a
minyoung1101 <my11612@naver.com>
2025-07-19 01:02:12 +0900

    Feat: 로그인/회원가입 유효성 검사 함수 수정 및 이벤트 등록

--- validation.js
+++ validation.js
@@ -1,16 +1,30 @@
-function validateEmail(e) {
-  const p = e.target.nextElementSibling || document.createElement("p");
-  if (!e.target.validity.valid) {
+function validateInput(e) {
+  const parentEl = e.target.parentElement;
+  const parentClassName = parentEl.className;
+  const targetLabel = parentEl.firstElementChild.textContent;
+  let p = document.querySelector(`.${parentClassName} p`);
+  if(!p) {
+    p = document.createElement("p");
     p.classList.add("invalid-input");
-    p.textContent = e.target.value === ""? "이메일을 입력해주세요.": "잘못된 이메일 형식입니다.";
+  }
+
+  const { validity } = e.target;
+  if (!validity.valid) {
+    if (validity.valueMissing) {
+      p.textContent = `${targetLabel}을 입력해주세요.`;
+    } else if (validity.typeMismatch) {
+      p.textContent = "잘못된 이메일 형식입니다.";
+    } else if (validity.tooShort) {
+      p.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    }
     e.target.after(p);
   } else {
     p.remove();
   }
 }
 
-const loginEmail = document.getElementById("login_email");
-if(loginEmail) loginEmail.addEventListener("focusout", (e) => validateEmail(e));
-
-const signupEmail = document.getElementById("signup_email");
-if(signupEmail) signupEmail.addEventListener("focusout", (e) => validateEmail(e));
+const inputIds = ["login_email", 'login_password', 'signup_email','signup_name','signup_password', 'signup_password_check'];
+inputIds.forEach(id=>{
+  const inputEl = document.getElementById(id);
+  inputEl && inputEl.addEventListener("focusout", (e) => validateInput(e));
+});
