3ccb48db2c1cf2e44af99441d377f5ef0f7d9d15
minyoung1101 <my11612@naver.com>
2025-07-19 03:03:54 +0900

    Feat: 회원가입 비밀번호 확인 기능 구현

--- validation.js
+++ validation.js
@@ -1,11 +1,18 @@
-function validateInput(e, targetLabel) {
+function validateInput(e, label) {
   const { validity } = e.target;
   if (!validity.valid) {
-    if (validity.valueMissing) return `${targetLabel}을 입력해주세요.`;
-    if (validity.typeMismatch) return "잘못된 이메일 형식입니다.";
-    if (validity.tooShort) return "비밀번호를 8자 이상 입력해주세요.";
+    if (validity.valueMissing) return `${label}을 입력해주세요`;
+    if (validity.typeMismatch) return "잘못된 이메일 형식입니다";
+    if (validity.tooShort) return "비밀번호를 8자 이상 입력해주세요";
   }
+  if(label==='비밀번호 확인' && !validatePWCheck()) return "비밀번호가 일치하지 않습니다";
+
   return "";
+}
+
+function validatePWCheck() {
+  const [pw,pwCheck] = document.querySelectorAll('input[id^=signup_password]');
+  return pw.value===pwCheck.value;
 }
 
 function showValidMessage(e) {
@@ -18,7 +25,7 @@ function showValidMessage(e) {
     p.classList.add("invalid-input");
   }
 
-  const errMsg = validateInput(e.target, targetLabel);
+  const errMsg = validateInput(e, targetLabel);
   if (errMsg) {
     p.textContent = errMsg;
     e.target.after(p);
