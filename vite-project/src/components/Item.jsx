f302dd56c251d3742f5f37c998fb969c2eeba078
Chamsol Kim <kcsol1005@gmail.com>
2025-07-24 16:03:18 +0900

    fix(Sprint5): 속성 이름 수정 : `class` -> `className`

--- vite-project/src/components/Item.jsx
+++ vite-project/src/components/Item.jsx
@@ -7,9 +7,9 @@ function Item({ title, price, likeCount }) {
     <div className="Item">
       <div className="Item-image"></div>
       <div className="Item-info">
-        <p class="Item-title">{title}</p>
-        <p class="Item-price">{priceString}</p>
-        <div class="Item-likes">
+        <p className="Item-title">{title}</p>
+        <p className="Item-price">{priceString}</p>
+        <div className="Item-likes">
           <img src={likeImg} alt="좋아요 갯수" />
           <span>{likeCount}</span>
         </div>
