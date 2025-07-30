97d3217d26b1da89f8067a74a8d67c5023bd7bcf
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-31 02:25:51 +0900

    feat: 404 페이지 추가

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Items from './pages/items/Items';
-import Board from './pages/board/Board';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Items from "./pages/items/Items";
+import Board from "./pages/board/Board";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/board" element={<Board />} />
       <Route path="/items" element={<Items />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
