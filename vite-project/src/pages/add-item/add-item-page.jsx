fa9ad93375cfce45c8ea1b2387449f9fb70a2cbd
Chamsol Kim <kcsol1005@gmail.com>
2025-07-31 15:53:10 +0900

    feat(Sprint6): 상품 등록 화면 기본 구조 구현

--- vite-project/src/pages/add-item/add-item-page.jsx
+++ vite-project/src/pages/add-item/add-item-page.jsx
@@ -1,5 +1,33 @@
+import Button from "../../components/Button";
+import Section from "../../components/section/section";
+import SectionHeader from "../../components/section/section-header";
+import SectionHeaderSize from "../../components/section/section-header-size";
+
 function AddItemPage() {
-  return <h1>Add Item Page</h1>;
+  return (
+    <Section spacing={24}>
+      <SectionHeader title={"상품 등록하기"}>
+        <Button disabled>등록</Button>
+      </SectionHeader>
+      <Section spacing={32}>
+        <Section>
+          <SectionHeader title={"상품 이미지"} size={SectionHeaderSize.SMALL} />
+        </Section>
+        <Section>
+          <SectionHeader title={"상품명"} size={SectionHeaderSize.SMALL} />
+        </Section>
+        <Section>
+          <SectionHeader title={"상품 소개"} size={SectionHeaderSize.SMALL} />
+        </Section>
+        <Section>
+          <SectionHeader title={"판매 가격"} size={SectionHeaderSize.SMALL} />
+        </Section>
+        <Section>
+          <SectionHeader title={"태그"} size={SectionHeaderSize.SMALL} />
+        </Section>
+      </Section>
+    </Section>
+  );
 }
 
 export default AddItemPage;
