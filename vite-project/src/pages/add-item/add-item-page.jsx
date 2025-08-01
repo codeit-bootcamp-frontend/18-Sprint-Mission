42610820707818a37f20e3ac3e010a184df5ac12
Chamsol Kim <kcsol1005@gmail.com>
2025-08-01 09:40:16 +0900

    feat(Sprint6): 상품 등록하기 - Text input component 구현

--- vite-project/src/pages/add-item/add-item-page.jsx
+++ vite-project/src/pages/add-item/add-item-page.jsx
@@ -1,35 +1,50 @@
+import styled from "styled-components";
 import AddingItemImageContainer from "../../components/add-item/adding-item-image-container";
 
 import Section from "../../components/section/section";
 import SectionHeader from "../../components/section/section-header";
 import SectionHeaderAction from "../../components/section/section-header-action";
 import SectionHeaderSize from "../../components/section/section-header-size";
+import TextInput from "../../components/text-input";
+
+const StyledItemsPage = styled.form`
+  padding-bottom: 69px;
+`;
 
 function AddItemPage() {
   return (
-    <Section spacing={24}>
-      <SectionHeader title={"상품 등록하기"}>
-        <SectionHeaderAction disabled>등록</SectionHeaderAction>
-      </SectionHeader>
-      <Section spacing={32}>
-        <Section>
-          <SectionHeader title={"상품 이미지"} size={SectionHeaderSize.SMALL} />
-          <AddingItemImageContainer />
-        </Section>
-        <Section>
-          <SectionHeader title={"상품명"} size={SectionHeaderSize.SMALL} />
-        </Section>
-        <Section>
-          <SectionHeader title={"상품 소개"} size={SectionHeaderSize.SMALL} />
-        </Section>
-        <Section>
-          <SectionHeader title={"판매 가격"} size={SectionHeaderSize.SMALL} />
-        </Section>
-        <Section>
-          <SectionHeader title={"태그"} size={SectionHeaderSize.SMALL} />
+    <StyledItemsPage>
+      <Section spacing={24}>
+        <SectionHeader title={"상품 등록하기"}>
+          <SectionHeaderAction disabled>등록</SectionHeaderAction>
+        </SectionHeader>
+        <Section spacing={32}>
+          <Section>
+            <SectionHeader
+              title={"상품 이미지"}
+              size={SectionHeaderSize.SMALL}
+            />
+            <AddingItemImageContainer />
+          </Section>
+          <Section>
+            <SectionHeader title={"상품명"} size={SectionHeaderSize.SMALL} />
+            <TextInput placeholder={"상품명을 입력해주세요"} />
+          </Section>
+          <Section>
+            <SectionHeader title={"상품 소개"} size={SectionHeaderSize.SMALL} />
+            <TextInput placeholder={"상품 소개를 입력해주세요"} multiline />
+          </Section>
+          <Section>
+            <SectionHeader title={"판매 가격"} size={SectionHeaderSize.SMALL} />
+            <TextInput placeholder={"판매 가격을 입력해주세요"} />
+          </Section>
+          <Section>
+            <SectionHeader title={"태그"} size={SectionHeaderSize.SMALL} />
+            <TextInput placeholder={"태그를 입력해주세요"} />
+          </Section>
         </Section>
       </Section>
-    </Section>
+    </StyledItemsPage>
   );
 }
 
