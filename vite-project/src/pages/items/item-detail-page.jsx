1735fa35d4c52570472673907b08b36d50d42ceb
Chamsol Kim <kcsol1005@gmail.com>
2025-08-05 08:26:26 +0900

    feat(Sprint7) 상품 클릭 시 상세 페이지 이동

---
+++ vite-project/src/pages/items/item-detail-page.jsx
@@ -0,0 +1,10 @@
+function ItemDetailPage() {
+  return (
+    <div>
+      <h1>Item Detail Page</h1>
+      {/* Add your item detail content here */}
+    </div>
+  );
+}
+
+export default ItemDetailPage;
