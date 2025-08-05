da3f3b57db1156eed09c87f6f2af22e6add8f824
Chamsol Kim <kcsol1005@gmail.com>
2025-08-05 20:16:11 +0900

    feat(Sprint7): 상품 상세 - '목록으로 돌아가기' 버튼 구현

--- vite-project/src/pages/items/item-detail-page.jsx
+++ vite-project/src/pages/items/item-detail-page.jsx
@@ -1,8 +1,17 @@
 import styled from "styled-components";
+import backImg from "../../assets/ic-arrow-back.svg";
+import Button from "../../components/button/button";
+import {
+  BUTTON_SIZE,
+  BUTTON_TYPE,
+} from "../../components/button/button-styles";
 import ItemComment from "../../components/item-comment/item-comment";
 import ItemDetail from "../../components/item-detail/item-detail";
 
 const StyledItemDetailPage = styled.div`
+  display: flex;
+  flex-direction: column;
+
   margin-bottom: 200px;
 
   @media (max-width: 767px) {
@@ -24,12 +33,29 @@ const Separator = styled.div`
   }
 `;
 
+const BackButton = styled(Button)`
+  align-self: center;
+  margin-top: 64px;
+
+  @media (max-width: 1199px) {
+    margin-top: 48px;
+  }
+
+  @media (max-width: 767px) {
+    margin-top: 40px;
+  }
+`;
+
 function ItemDetailPage() {
   return (
     <StyledItemDetailPage>
       <ItemDetail />
       <Separator />
       <ItemComment />
+      <BackButton size={BUTTON_SIZE.medium} type={BUTTON_TYPE.pill}>
+        목록으로 돌아가기
+        <img src={backImg} alt="되돌아가기" />
+      </BackButton>
     </StyledItemDetailPage>
   );
 }
