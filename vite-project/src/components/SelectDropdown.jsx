a1221f280ecc39a2697db8c474629085ec83f09f
BaeZzi813 <woehd8252@gmail.com>
2025-08-06 02:42:26 +0900

    fix: select 이미지 변경 목적 모바일 버전에서는 드롭다운 컴포넌트 생성 후 수정

---
+++ vite-project/src/components/SelectDropdown.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from "react";
+import selectIcon from "../assets/ic-sort.svg";
+
+export default function SelectDropdown({ onChange, value }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const options = [
+    { label: "최신순", value: "recent" },
+    { label: "좋아요순", value: "favorite" },
+  ];
+
+  const selected = options.find((opt) => opt.value === value);
+
+  const handleSelect = (option) => {
+    onChange({ target: { value: option.value } });
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <>
+      <div ref={dropdownRef} className="select-dropdown">
+        <div className="selected-option" onClick={() => setIsOpen(!isOpen)}>
+          <img src={selectIcon} alt="드롭다운 아이콘" />
+        </div>
+
+        {isOpen && (
+          <ul className="option-list">
+            {options.map((option) => (
+              <li key={option.value} onClick={() => handleSelect(option)}>
+                {option.label}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+}
--- vite-project/src/components/TotalProducts.jsx
+++ vite-project/src/components/TotalProducts.jsx
@@ -2,6 +2,7 @@ import likeIcon from "../assets/ic-heart.svg";
 import searchIcon from "../assets/ic-search.svg";
 import { Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+import SelectDropdown from "./SelectDropdown";
 
 function TotalProducts({
   totalProducts,
@@ -55,10 +56,7 @@ function TotalProducts({
                 onChange={onKeywordChange}
               />
             </div>
-            <select onChange={onOrderChange} value={orderBy}>
-              <option value="recent">최신순</option>
-              <option value="favorite">좋아요순</option>
-            </select>
+            <SelectDropdown value={orderBy} onChange={onOrderChange} />
           </div>
         </div>
       ) : null}
