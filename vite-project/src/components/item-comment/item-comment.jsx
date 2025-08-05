9c59b120184ab3a53840d3ba406632cf43002e8a
Chamsol Kim <kcsol1005@gmail.com>
2025-08-05 17:20:55 +0900

    feat(Sprint7): 상품 상세 - 상품 정보와 comment를 구분하는 Separator component 구현

---
+++ vite-project/src/components/item-comment/item-comment.jsx
@@ -0,0 +1,5 @@
+function ItemComment() {
+  return <h2>Comments</h2>;
+}
+
+export default ItemComment;
--- vite-project/src/pages/items/item-detail-page.jsx
+++ vite-project/src/pages/items/item-detail-page.jsx
@@ -1,9 +1,27 @@
+import styled from "styled-components";
+import ItemComment from "../../components/item-comment/item-comment";
 import ItemDetail from "../../components/item-detail/item-detail";
+
+const Separator = styled.div`
+  width: 100%;
+  border-bottom: 1px solid var(--color-cool-gray-200);
+  margin: 40px 0;
+
+  @media (max-width: 1199px) {
+    margin-top: 32px;
+  }
+
+  @media (max-width: 767px) {
+    margin: 24px 0;
+  }
+`;
 
 function ItemDetailPage() {
   return (
     <div>
       <ItemDetail />
+      <Separator />
+      <ItemComment />
     </div>
   );
 }
