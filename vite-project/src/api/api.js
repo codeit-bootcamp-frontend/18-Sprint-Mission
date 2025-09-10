265a32bdf14977d188e423e0b17a20b2d83aa673
jeschun <jeschun54@gmail.com>
2025-09-10 18:47:57 +0900

    ♻️ : refactor fetch->axios 로 전환

--- vite-project/src/api/api.js
+++ vite-project/src/api/api.js
@@ -1,3 +1,5 @@
+import instance from "./axiosInstance";
+
 export async function getLists({
   page = 1,
   pageSize = 10,
@@ -5,12 +7,11 @@ export async function getLists({
   keyword = "",
 }) {
   const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`;
-  const res = await fetch(
-    `https://panda-market-api.vercel.app/products?${query}`
-  );
-  if (!res.ok) {
-    throw new Error("요청 실패");
+
+  try {
+    const { data } = await instance.get(`/products?${query}`);
+    return data;
+  } catch {
+    throw new Error("상품 목록을 가져오는데 실패했습니다.");
   }
-  const body = await res.json();
-  return body;
 }
