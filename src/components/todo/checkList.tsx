07191899efc008a895098ce1879de19f52c413ec
Imhwitae <ghkdgnlxo2@gmail.com>
2025-09-17 12:01:40 +0900

    design: 체크리스트 완료 상태 디자인 추가

--- src/components/todo/checkList.tsx
+++ src/components/todo/checkList.tsx
@@ -1,12 +1,31 @@
 import Image from "next/image";
 import styles from "./checkList.module.css";
 import notCheck from "../../../public/not_check.svg";
+import checkedIcon from "../../../public/checked.svg";
 
-export default function CheckList() {
+export default function CheckList({
+  name,
+  isCompleted,
+}: {
+  name: string;
+  isCompleted: boolean;
+}) {
   return (
-    <div className={styles.check_list_container}>
-      <Image src={notCheck} width={32} height={32} alt="체크안됨" />
-      <p className={styles.check_list_text}>비타민 챙겨먹기</p>
+    <div
+      className={
+        isCompleted
+          ? `${styles.check_list_container} ${styles.checked}`
+          : styles.check_list_container
+      }
+    >
+      <Image
+        className={styles.check_btn}
+        src={isCompleted ? checkedIcon : notCheck}
+        width={32}
+        height={32}
+        alt={isCompleted ? "완료" : "미완료"}
+      />
+      <p>{name}</p>
     </div>
   );
 }
--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import CheckList from "./checkList";
 import styles from "./todo.module.css";
+import todoImg from "../../../public/todo.png";
+import { todoData } from "@/mock";
 
 export default function Todo() {
   return (
     <div className={styles.todo_container}>
-      <Image src="/todo.png" width={101} height={36} alt="todo 이미지" />
-      <CheckList />
+      <Image src={todoImg} width={101} height={36} alt="todo 이미지" />
+      {todoData.map((todo) => (
+        <CheckList
+          key={todo.id}
+          name={todo.name}
+          isCompleted={todo.isCompleted}
+        />
+      ))}
     </div>
   );
 }
