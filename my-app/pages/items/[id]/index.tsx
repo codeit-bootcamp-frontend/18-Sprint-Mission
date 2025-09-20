0c4e0217ca8cd50456f1f7c1898460725f55d933
Chamsol Kim <kcsol1005@gmail.com>
2025-09-20 21:16:40 +0900

    feat(Sprint10): Todo detail page UI 구현

--- my-app/pages/items/[id]/index.tsx
+++ my-app/pages/items/[id]/index.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/button/button";
+import { ButtonType } from "@/components/button/button-type";
 import TodoDetailTitle from "@/components/todo/todo-detail-title";
 import { getTodo } from "@/libs/apis/todo";
 import styles from "@/styles/item.module.css";
@@ -15,17 +17,47 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 export default function Page({ todo }: { todo: Todo }) {
-  const handleTitleClick = (todo: Todo) => {
-    console.log("Title clicked:", todo);
-  };
-
   if (!todo) {
     return <div>Todo not found</div>;
   }
 
+  const handleTitleClick = (todo: Todo) => {
+    console.log("Title clicked:", todo);
+  };
+
+  const handleEditClick = () => {};
+
+  const handleDeleteClick = () => {};
+
   return (
-    <div className={styles.content}>
-      <TodoDetailTitle todo={todo} onClick={handleTitleClick} />
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <TodoDetailTitle todo={todo} onClick={handleTitleClick} />
+        <div className={styles.imageMemoContainer}>
+          <div className={styles.imageContainer}>
+            <img
+              src="/images/todo-detail-image-background.svg"
+              alt="background"
+            />
+          </div>
+          <div className={styles.memoContainer}>
+            <span>Memo</span>
+            <p>
+              {
+                "내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내부 스크롤이 이루어집니다. 내용이 최대로 늘어날 때 다음과 같이 보이며 내"
+              }
+            </p>
+          </div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button buttonType={ButtonType.edit} onClick={handleEditClick}>
+            수정 완료
+          </Button>
+          <Button buttonType={ButtonType.delete} onClick={handleDeleteClick}>
+            삭제하기
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
