e887d9ea90b4df3f97b9996f3e084c80019cdca2
Chamsol Kim <kcsol1005@gmail.com>
2025-09-20 17:41:41 +0900

    feat(Sprint10): 배경색 설정

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <GlobalNavBar />
-      <main>
+      <main className={styles.main}>
         <div className={styles.container}>
           <Component {...pageProps} />
         </div>
