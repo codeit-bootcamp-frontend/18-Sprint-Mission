c7aee71050245f1a27bcf56d3071b9f091db612b
jeschun <jeschun54@gmail.com>
2025-09-20 13:17:34 +0900

    💄 style: checklist,list,editor 레이아웃 개선, 스타일 수정

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,12 +1,45 @@
+import { useState, useRef } from "react";
 import Editor from "src/components/editor";
-import List from "src/components/List";
+import List from "src/components/list";
 import styles from "./index.module.css";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "공부하기",
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "게임하기",
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "잠자기",
+  },
+];
+
 export default function Home() {
+  const [todos, setTodos] = useState(mockData);
+
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <>
       <div className={styles.container}>
-        <Editor />
+        <Editor onCreate={onCreate} />
         <List />
       </div>
     </>
--- my-app/src/components/editor.tsx
+++ my-app/src/components/editor.tsx
@@ -1,10 +1,38 @@
 import styles from "./editor.module.css";
+import { useState, useRef } from "react";
 
-export default function Editor() {
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const contentRef = useRef<HTMLInputElement>(null);
+
+  const onKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onSubmit();
+  };
+
+  const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      return alert("내용을 입력해주세요");
+    }
+    onCreate();
+    setContent("");
+  };
   return (
     <div className={styles.container}>
-      <input className={styles.input} placeholder="할 일을 입력해주세요" />
-      <button className={styles.button}>+ 추가하기</button>
+      <input
+        ref={contentRef}
+        onKeyDown={onKeydown}
+        value={content}
+        onChange={onChangeContent}
+        className={styles.input}
+        placeholder="할 일을 입력해주세요"
+      />
+      <button className={styles.button} onClick={onSubmit}>
+        + 추가하기
+      </button>
     </div>
   );
 }
